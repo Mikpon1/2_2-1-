75110564f6830221c0d2a7d926064721754fe176
Mikpon1 <127319253+Mikpon1@users.noreply.github.com>
2023-03-22 14:07:53 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    double a=7;
+    double b=7;
+    double sum = a+b;
+    double roz = a-b;
+    double iloczyn = a*b;
+    double iloraz = a/b;
+    System.out.println("suma = " + sum + "różnica = " + roz + "iloczyn = " + iloczyn + "iloraz = " + iloraz);
+  }
+}
